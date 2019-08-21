5465b887acc1238386cd54c5f74412128a2ca81c
Michael Mooney <51172274+MichaelCMooney@users.noreply.github.com>
2019-08-21 12:02:49 -0400

    Uploaded all files for RDP client

---
+++ Form1.vb
@@ -0,0 +1,49 @@
+﻿Imports MSTSCLib
+
+Public Class RDPBase
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+
+        Try
+
+            rdpView.Server = txtServer.Text
+            rdpView.UserName = txtUserName.Text
+
+            Dim isSecured As IMsTscNonScriptable =
+               DirectCast(rdpView.GetOcx(), IMsTscNonScriptable)
+
+            isSecured.ClearTextPassword = txtPassword.Text
+
+            rdpView.Connect()
+
+        Catch ex As Exception
+
+            MessageBox.Show("Cannot Connect", "Cannot Connect to: " _
+               + txtServer.Text + " Reason:  " + ex.Message,
+               MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        End Try
+    End Sub
+
+    Private Sub BtnDisconnect_Click(sender As Object, e As EventArgs) Handles btnDisconnect.Click
+        Try
+
+            If rdpView.Connected.ToString() = "1" Then
+
+                rdpView.Disconnect()
+
+            End If
+
+        Catch ex As Exception
+
+            MessageBox.Show("Cannot Disconnect",
+               "Cannot Disconnect from: " _
+               + txtServer.Text + " Reason: " + ex.Message,
+               MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        End Try
+    End Sub
+End Class
---
+++ RDP_Client/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RDPBase
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(RDPBase))
+        Me.rdpView = New AxMSTSCLib.AxMsTscAxNotSafeForScripting()
+        Me.txtServer = New System.Windows.Forms.TextBox()
+        Me.txtUserName = New System.Windows.Forms.TextBox()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.btnDisconnect = New System.Windows.Forms.Button()
+        CType(Me.rdpView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'rdpView
+        '
+        Me.rdpView.Enabled = True
+        Me.rdpView.Location = New System.Drawing.Point(3, 55)
+        Me.rdpView.Name = "rdpView"
+        Me.rdpView.OcxState = CType(resources.GetObject("rdpView.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.rdpView.Size = New System.Drawing.Size(1005, 559)
+        Me.rdpView.TabIndex = 0
+        '
+        'txtServer
+        '
+        Me.txtServer.Location = New System.Drawing.Point(15, 29)
+        Me.txtServer.Name = "txtServer"
+        Me.txtServer.Size = New System.Drawing.Size(146, 20)
+        Me.txtServer.TabIndex = 1
+        '
+        'txtUserName
+        '
+        Me.txtUserName.Location = New System.Drawing.Point(180, 29)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.Size = New System.Drawing.Size(146, 20)
+        Me.txtUserName.TabIndex = 2
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(347, 29)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(169, 20)
+        Me.txtPassword.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Server:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(177, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Username:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(344, 15)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Password:"
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(548, 27)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnConnect.TabIndex = 7
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'btnDisconnect
+        '
+        Me.btnDisconnect.Location = New System.Drawing.Point(629, 27)
+        Me.btnDisconnect.Name = "btnDisconnect"
+        Me.btnDisconnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnDisconnect.TabIndex = 8
+        Me.btnDisconnect.Text = "Disconnect"
+        Me.btnDisconnect.UseVisualStyleBackColor = True
+        '
+        'RDPBase
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1013, 618)
+        Me.Controls.Add(Me.btnDisconnect)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.txtUserName)
+        Me.Controls.Add(Me.txtServer)
+        Me.Controls.Add(Me.rdpView)
+        Me.Name = "RDPBase"
+        Me.Text = "Michael Mooney's RDP Client"
+        CType(Me.rdpView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents rdpView As AxMSTSCLib.AxMsTscAxNotSafeForScripting
+    Friend WithEvents txtServer As TextBox
+    Friend WithEvents txtUserName As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents btnDisconnect As Button
+End Class
